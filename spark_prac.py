f0e92e94985f26dc1b3b08823c103cc235e76f7f
daulet <shabakovdaulet@gmail.com>
2026-05-02 03:25:33 +0200

    practice

--- spark_prac.py
+++ spark_prac.py
@@ -1,1 +1,22 @@
-print("hi")
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder
+    .appName("MyFirstSparkPractice")
+    .getOrCreate()
+)
+
+
+data = [
+    (101, "ShadowNinja", 500),
+    (102, "GamerPro", 150),
+    (103, "NoobMaster", 1000)
+]
+
+columns = ["player_id", "nickname", "amount_spent"]
+
+df = spark.createDataFrame(data, columns)
+
+df.show()
+
+spark.stop()
